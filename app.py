34e7cf1aeb6255d391ce5428500711d2efdbca50
we32693269 <eshetuyimer538@gmail.com>
2026-05-28 08:28:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, send_file
+from werkzeug.utils import secure_filename
+import os
 import sqlite3
 from datetime import datetime
 from reportlab.pdfgen import canvas
