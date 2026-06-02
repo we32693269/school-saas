e563e72cf2e8942fffbaac58e1d4b22ce2da950a
we32693269 <eshetuyimer538@gmail.com>
2026-06-02 11:14:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -188,6 +188,8 @@ def search():
 #=============== LIST STUDENTS ==================
 @app.route("/list")
 def list_students():
+    if "user" not in session:
+    return redirect("/login")
     conn = sqlite3.connect("school.db")
     cursor = conn.cursor()
 
