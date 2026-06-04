10ec05b6eba131aa29a6dac826b8f5edf022bbd9
we32693269 <eshetuyimer538@gmail.com>
2026-06-05 02:14:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,16 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 def init_db():
     conn = sqlite3.connect("school.db")
     cursor = conn.cursor()
-
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name NULL,NULL
+        class NULL,NULL
+        age NULL,NULL
+        gender NULL,NULL
+        photo NULL,NULL
+    )
+    """)
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
