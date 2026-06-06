537322226d8dd5e555147ccc7596bc96e2666a32
we32693269 <eshetuyimer538@gmail.com>
2026-06-06 21:06:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,28 +2,29 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-# Home
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Login
+# 🔐 LOGIN
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
 
-        # simple demo login (later database)
+        # demo users (later database)
         if email == "admin@gmail.com" and password == "1234":
             return redirect('/dashboard')
 
+        return "Invalid login ❌"
+
     return render_template('login.html')
 
-# Dashboard
+# 📊 DASHBOARD
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
