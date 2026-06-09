866f376e1c450e2f941b0c9b6b1abca1c16085fc
we32693269 <eshetuyimer538@gmail.com>
2026-06-09 20:05:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -165,8 +165,9 @@ def receipt(id):
     file_name = f"receipt_{id}.pdf"
     pdf = canvas.Canvas(file_name)
    
-if os.path.exists(logo_path):
-    pdf.drawImage(logo_path, 260, 760, width=80, height=80)
+    if os.path.exists(logo_path):
+        pdf.drawImage(logo_path, 260, 760, 
+    width=80, height=80)
  # ================= BORDER =================
     pdf.rect(30, 30, 540, 780)
 
