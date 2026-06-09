3cc078f4690fd5581d0061ea4d19cd3cb61dd0d9
we32693269 <eshetuyimer538@gmail.com>
2026-06-09 19:43:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -168,7 +168,8 @@ def receipt(id):
  # ================= BORDER =================
     pdf.rect(30, 30, 540, 780)
 
-
+if os.path.exists(logo_path):
+    pdf.drawImage(logo_path, 260, 760, width=80, height=80)
 # ================= SCHOOL NAME =================
     pdf.setFont("Helvetica-Bold", 18)
     pdf.drawCentredString(300, 740, "MY SCHOOL")
