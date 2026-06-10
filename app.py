10113c087f1317aa6ab961b2aae5543949fa1570
we32693269 <eshetuyimer538@gmail.com>
2026-06-10 10:03:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -234,16 +234,13 @@ def receipt(id):
     pdf.rect(30, 30, 540, 780)
 
     # ================= LOGO =================
-    logo = "static/logo.png"
+    logo = os.path.join("static", "logo.png")
 
-    if os.path.exists(logo):
-        pdf.drawImage(
-            logo,
-            260,
-            760,
-            width=80,
-            height=80
-        )
+if os.path.exists(logo):
+    pdf.drawImage(logo, 260, 760, width=80, height=80)
+else:
+    pdf.setFont("Helvetica", 8)
+    pdf.drawString(260, 760, "NO LOGO")
 
     # ================= SCHOOL NAME =================
     pdf.setFont("Helvetica-Bold", 18)
