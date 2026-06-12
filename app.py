2b988daebf3fc4e7440e9416f8386b729edb1197
we32693269 <eshetuyimer538@gmail.com>
2026-06-12 19:46:45 +0300

    Add users table to database schema

--- app.py
+++ app.py
@@ -62,6 +62,12 @@ def init_db():
         date TEXT
     )
     """)
+    CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        password TEXT,
+        role TEXT
+    );
 
     # ================= SETTINGS =================
     c.execute("""
