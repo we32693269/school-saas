530d3ed7f47bf358ea198ff682ad7f4680bbc2a0
we32693269 <eshetuyimer538@gmail.com>
2026-06-13 06:50:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ def student_profile(id):
 # ================= DASHBOARD =================
 @app.route('/dashboard')
 def dashboard():
-    if not session.get('admin'):
+    if session.get('role') != 'admin':
         return redirect('/login')
 
     search = request.args.get('search', '')
