ad8306f9ddf6ae4cd07a212157df011ccb1bd9f8
we32693269 <eshetuyimer538@gmail.com>
2026-06-19 12:43:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import psycopg2
-DATABASE_URL = "postgresql://postgres:YOUR_PASSWORD@db.eeiuppapncwnhwzonakg.supabase.co:5432/postgres"
+DATABASE_URL = "postgresql://postgres:wr32693269@abc@db
+.eeiuppapncwnhwzonakg.supabase.co:5432/postgres"
 def get_db():
     return psycopg2.connect(DATABASE_URL)
 import os
